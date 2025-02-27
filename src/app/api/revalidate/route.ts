185422cf091f450ec427e044c194d2c30472e83b
mauricio-cantu <mauriciocantujoc@gmail.com>
2025-02-27 14:38:48 -0300

    protect revalidate route with secret

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,8 +1,12 @@
-import { NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
+import { NextResponse } from "next/server";
 
-export async function POST() {
-  revalidateTag("prismic");
+export async function POST(request: Request) {
+  const body = await request.json();
+  if (body.secret === process.env.PRISMIC_WEBHOOK_SECRET) {
+    revalidateTag("prismic");
+    return NextResponse.json({ revalidated: true, now: Date.now() });
+  }
 
-  return NextResponse.json({ revalidated: true, now: Date.now() });
+  return NextResponse.json({ revalidated: false });
 }
