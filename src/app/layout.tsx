556eac2a585b01ddfc10a7480a5f4251f2423d85
mauricio-cantu <mauriciocantujoc@gmail.com>
2025-02-26 21:17:43 -0300

    adds Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Footer } from "@/components/Footer";
 import Header from "@/components/Header";
 import { repositoryName } from "@/prismicio";
 import { PrismicPreview } from "@prismicio/next";
+import { Analytics } from "@vercel/analytics/react";
 import { indieFlower, lato } from "./fonts/localFont";
 import "./globals.css";
 import { Providers } from "./providers";
@@ -21,6 +22,7 @@ export default async function RootLayout({
           <main className="grid h-screen grid-rows-[auto_1fr_auto] bg-background text-foreground">
             <Header socials={socials} navigationItems={navigationItems} />
             {children}
+            <Analytics />
             <Footer />
           </main>
           <PrismicPreview repositoryName={repositoryName} />
